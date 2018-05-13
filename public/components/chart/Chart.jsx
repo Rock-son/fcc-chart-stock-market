652e7d259fa69a3868e96fd7157e0bdd2d643807
rok <roky.zz@gmail.com>
2018-05-14 00:04:05 +0200

    Chart component - added stock removeHandler + UI

--- public/components/chart/Chart.jsx
+++ public/components/chart/Chart.jsx
@@ -2,12 +2,12 @@
 
 import React from "react";
 
-import request from "../apis/api";
+import axios from "../../scripts/api";
 
 // import chart from "./_chart";
 
 export default class Content extends React.Component {
-	static handleClick(e) {
+	static selectAllOnEnter(e) {
 		e.target.select();
 	}
 
@@ -15,18 +15,21 @@ export default class Content extends React.Component {
 		super();
 
 		this.state = { input: "", data: "", stocks: [], hasError: false, error: "" };
+		this.input = React.createRef();
+		this.searchBtn = React.createRef();
+
 		this.handleEnterPress = this.handleEnterPress.bind(this);
 		this.handleSearch = this.handleSearch.bind(this);
-		this.handleClick = this.handleClick.bind(this);
+		this.selectAllOnEnter = this.selectAllOnEnter.bind(this);
 		this.handleInput = this.handleInput.bind(this);
+		this.removeStock = this.removeStock.bind(this);
 	}
 
 	componentDidMount() {
-
+		this.input.current.focus();
 	}
-	componentDidCatch(error, info) {
+	componentDidCatch(error/* , info */) {
 		// Display fallback UI
-		console.log(error, info);
 		this.setState({ hasError: true, error });
 	}
 
@@ -36,19 +39,37 @@ export default class Content extends React.Component {
 		}
 	}
 
-	handleSearch(e) {
+	removeStock(e) {
 		e.preventDefault();
-		/* request.addStock(this.state.input)
-			.then()
-			.catch(); */
-		if (this.state.stocks.indexOf(this.state.input) === -1) {
-			this.setState(prevState => ({ stocks: [...prevState.stocks, this.state.input] }));
+		if ((e.keyCode === 13 || e.type === "click") && ((e.currentTarget || {}).id || "").trim()) {
+			const stock = e.currentTarget.id;
+			axios.removeStock(stock)
+				.then(() => {
+					const stockIdx = this.state.stocks.indexOf(stock);
+					if (stockIdx > -1) {
+						this.setState(prevState => ({ stocks: prevState.stocks.slice(0, stockIdx).concat(prevState.stocks.slice(stockIdx + 1)) }));
+						this.input.current.click();
+					}
+				})
+				.catch(error => this.setState({ hasError: true, error }));
 		}
 	}
-
-	handleClick(e) {
+	handleSearch(e) {
 		e.preventDefault();
-		this.constructor.handleClick(e);
+		axios.addStock(this.state.input)
+			.then(response => console.log(response.data))
+			.catch(err => console.error(err));
+
+		if (this.state.stocks.indexOf(this.state.input) === -1 && this.state.input.trim()) {
+			this.setState(prevState => ({ stocks: [...prevState.stocks, this.state.input] }));
+			this.input.current.click();
+		}
+		this.input.current.click();
+	}
+
+	selectAllOnEnter(e) {
+		e.preventDefault();
+		this.constructor.selectAllOnEnter(e);
 	}
 
 	handleInput(e) {
@@ -62,18 +83,20 @@ export default class Content extends React.Component {
 		return (
 			<div className="chart__form" >
 				<div className="chart__form__container" >
-					<h2 className="chart__form__container__header" >Syncs stocks in realtime across clients</h2>
+					<h4 className="chart__form__container__header" >Syncs stocks in realtime across clients</h4>
 					<div className="chart__form__container__inputs" >
-						<input type="text" className="chart__form__container__inputs__text" placeholder="Stock code" value={this.state.input} onClick={this.handleClick} onChange={this.handleInput} />
-						<input type="button" className="chart__form__container__inputs__button" value="Search" onClick={this.handleSearch} />
+						<input type="text" className="chart__form__container__inputs__text" ref={this.input} placeholder="Stock code" value={this.state.input} onClick={this.selectAllOnEnter} onChange={this.handleInput} onKeyUp={this.handleEnterPress} />
+						<input type="button" className="chart__form__container__inputs__button" ref={this.searchBtn} value="Search" onClick={this.handleSearch} />
 					</div>
 				</div>
 				{(this.state.hasError) ?
-					(<h2 className="content__cards__error">{`There was an error (${this.state.error}), please try again later!`}</h2>) :
+					(<h2 className="content__cards__error">{this.state.error}</h2>) :
 
 					(this.state.stocks.map(stock => (
-						<div key={stock} className="chart__form__container__stocks">{stock}
-							<div className="chart__form__container__stocks_description">Facebook Inc. (FB) Prices, Dividends, Splits and Trading Volume</div>
+						<div key={stock} className="chart__form__container">
+							<div id={stock} role="button" tabIndex={0} className="chart__form__container__close" onClick={this.removeStock} onKeyUp={this.removeStock}>x</div>
+							<h4 className="chart__form__container__header" >{stock}</h4>
+							<div className="chart__form__container__description">Facebook Inc. (FB) Prices, Dividends, Splits and Trading Volume</div>
 						</div>))
 					)
 				}
