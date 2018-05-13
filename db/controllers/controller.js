b42e3e78e44980b7765380b00a11784b0f8bcf18
rok <roky.zz@gmail.com>
2018-05-13 23:46:55 +0200

    db controller - added addStock and removeStock logic

--- db/controllers/controller.js
+++ db/controllers/controller.js
@@ -13,65 +13,33 @@ exports.getStocks = function (city) {
 };
 
 
-exports.addStock = function (req, res, next, data) {
-
-	StockChart.findOne({ 'bar.id': data.id }, function(err, result) {
-		if (err) { return res.status(400).send(err); }
-
-		// IF BAR exists
-		if (result) {
-			// if USER exist in bar array - return unchanged, else add and save
-			if (result.bar.users.indexOf(data.user) > -1) {
-				return res.status(200).send({ users: result.bar.users.slice(), id: data.id });
-			} else {
-				result.bar.users = result.bar.users.slice().concat(data.user);
-				result.save(function (err) {
-					if (err) { return res.status(400).send(err); }
-
-					return res.status(200).send({ users: result.bar.users, id: data.id });
-				});
-			}
-		} else {
-			// IF BAR NOT exists - make and save new bar
-			const newBar = new StockChart({
-				city: data.city,
-				bar: {
-					id: data.id,
-					users: [data.user]
-				}
-			});
-			newBar.save(function (err) {
-				if (err) { return res.status(400).send(err); }
-
-				return res.status(200).send({ users: newBar.bar.users.slice(), id: data.id });
-			})
-		}
-	})
-	.catch(err => res.status(400).send(err));
-};
-
-exports.removeGoingUsers = function (req, res, next, data) {
-
-	StockChart.findOne({ "bar.id": data.id }, function(err, result) {
+exports.addStock = function (req, res, next, code) {
+	return StockChart.findOne({ "code": code }, function(err, result) {
 		if (err) { return err; }
 
-		// if BAR exists
-		if (result) {
-			// if USER exist - remove them and save, else return same array
-			let idx = null;
-			if ((idx = result.bar.users.indexOf(data.user)) > -1) {
-				result.bar.users = result.bar.users.slice(0, idx).concat(result.bar.users.slice(idx + 1));
-				result.save(function (err) {
-					if (err) { return res.status(402).send(err); }
+		// IF STOCK DOES NOT exist - save it else return next()
+		if (!result) {
+			const newStock = new StockChart({
+				name: "test",
+				code
+			});
+			newStock.save(function (err) {
+				if (err) { return err; }
 
-					return res.status(200).send({ users: result.bar.users.slice(0), id: data.id });
-				});
-				return;
-			} else {
-				return res.status(200).send({ users: [], id: "" });
-			}
+				return "test";
+			});
+		} else {
+			return "test";
 		}
-		// IF BAR NOT exists
-		return res.status(200).send({ users: [], id: "" });
-	});
+	})
+	.catch(err => err);
+};
+
+exports.removeStock = function (req, res, next, code) {
+	return StockChart.remove({ "code": code }, function(err) {
+		if (err) { return err; }
+
+		return "";
+	})
+	.catch(err => err);
 };
