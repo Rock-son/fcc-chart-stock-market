9e4df9513d1240e43c0e7a7f010d62865426ef75
rok <roky.zz@gmail.com>
2018-05-17 00:00:22 +0200

    StockChart - changed Schema (no id in values + added quote field)

--- db/models/StockChart.js
+++ db/models/StockChart.js
@@ -8,10 +8,19 @@ const { Schema } = require("mongoose");
 const stockChart = new Schema({
 
 	name: {
-		type: String, required: true, trim: true, lowercase: true
+		type: String, required: true, trim: true
 	},
 	code: {
-		type: String, required: true, trim: true, lowercase: true
+		type: String, required: true, trim: true, upperCase: true
+	},
+	quote: {
+		type: Object
+	},
+	chart: {
+		id: { type: String, trim: true, upperCase: true },
+		values: {
+			type: Schema.Types.Mixed
+		}
 	},
 	updatedUTC: { type: Date }
 });
