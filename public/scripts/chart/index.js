bc548b742e1460bffe01699f5cf21e92993babbd
rok <roky.zz@gmail.com>
2018-05-19 03:31:35 +0200

    chart index.js - refactoring due to change in removing stocks logic

--- public/scripts/chart/index.js
+++ public/scripts/chart/index.js
@@ -5,11 +5,11 @@ import * as d3 from "d3";
 import plot from "./drawPlot";
 
 
-export default function (pollData) {
+export default function (pollData, initialize = true, removeStock = null) {
 	// container object's width and height
 	const winSize = +window.innerWidth;
-	const w = winSize > 660 ? winSize * 0.84 : winSize * 0.95; // same as in responsive CSS
-	const h = +window.innerHeight * 0.65;
+	const w = winSize > 660 ? winSize * 0.75 : winSize * 0.95; // same as in responsive CSS
+	const h = +window.innerHeight * 0.6;
 	const margin = {
 		top: 60,
 		bottom: 60,
@@ -32,43 +32,49 @@ export default function (pollData) {
 	}];
 	*/
 	/* eslint-disable indent */
-	function onData() {
-		// const parseTime = d3.timeFormat("%d-%b-%y");
+		const parseTime = d3.timeFormat("%d.%b");
 		const data = pollData.map(stock => ({ id: stock.id, values: stock.values.map(value => ({ date: new Date(value.date), price: +value.price })) }));
-		const description = "";
+		const description = "Powered by http://iextrading.com";
+		let chart = null;
+		let tooltip = null;
 		const x = d3.scaleTime()
 			.domain([	d3.min(data, c => d3.min(c.values, d => d.date)),
 						d3.max(data, c => d3.max(c.values, d => d.date))])
 			.range([0, width]);
 		const y = d3.scaleLinear()
-						.domain([d3.min(data, c => d3.min(c.values, d => +d.price)) - 100,
+						.domain([0/* d3.min(data, c => d3.min(c.values, d => +d.price)) - 100 */,
 								d3.max(data, c => d3.max(c.values, d => +d.price)) + 50])
 						.range([height, 0]);
 		const z = d3.scaleOrdinal(d3.schemeCategory10)
 						.domain(data.map(d => d.id));
 		const line = d3.line()
 						.x(d => x(d.date))
-						.y(d => y(d.price));
+						.y(d => y(d.price))
+        				.curve(d3.curveLinear);
 		const yGridLines = d3.axisLeft()
 								.scale(y)
 								.tickSize(-width, 0, 0)
 								.tickFormat("");
 		const xAxis = d3.axisBottom()
-							.scale(x);
-		const yAxis = d3.axisLeft()
+							.scale(x)
+							.tickFormat(parseTime);
+		const yAxis = d3.axisRight()
 							.scale(y)
+							.tickSize(3, 0, 0)
 							.ticks(d3.max(data, c => d3.max(c.values, d => d.data)));
+
+	if (initialize === true) {
 		const svg = d3.select("body")
 						.append("svg")
 						.attr("id", "chart")
 						.attr("width", w)
 						.attr("height", h);
-		const chart = svg.append("g")
-							.classed("display", true)
-							.attr("transform", `translate(${margin.left}, ${margin.top})`);
-		const tooltip = d3.select('body')
-							.append('div')
-							.classed("tooltip", true);
+		chart = svg.append("g")
+						.classed("display", true)
+						.attr("transform", `translate(${margin.left}, ${margin.top})`);
+		tooltip = d3.select('body')
+						.append('div')
+						.classed("tooltip", true);
 
 		chart.append('text')
 			.classed("chart-title", true)
@@ -79,8 +85,12 @@ export default function (pollData) {
 			.attr("y", -15)
 			.attr("transform", "translate(0,0)")
 			.style("text-anchor", "middle");
+	} else {
+		chart = d3.select("g.display");
+	}
 
 		plot.call(chart, {
+			removeStock,
 			height,
 			width,
 			tooltip,
@@ -95,9 +105,7 @@ export default function (pollData) {
 			yScale: y,
 			zScale: z,
 			gridlines: yGridLines,
-			initialize: true
+			initialize
 		});
-	}
-	onData();
 }
 /* eslint-enable */
