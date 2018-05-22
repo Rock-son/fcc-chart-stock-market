d39c8dc5b85d25590b12b0ac9046ed295e7fd254
rok <roky.zz@gmail.com>
2018-05-22 23:33:28 +0200

    mouseEvents - finished tootlip styling and handling updating stocks

--- public/scripts/chart/helpers/mouseEvents.js
+++ public/scripts/chart/helpers/mouseEvents.js
@@ -4,7 +4,7 @@ import * as d3 from "d3";
 
 let spread = null;
 
-export function mouseMove(data, xScale, yScale) {
+export function mouseMove(data, params) {
 	const height = d3.select(".zoom").attr("height");
 	const width = d3.select(".zoom").attr("width");
 
@@ -13,34 +13,38 @@ export function mouseMove(data, xScale, yScale) {
 
 	spread = spread || width / data.dates.length;
 	const idx = Math.round((d3.event.offsetX - offsetX) / spread) - 1;
+	const timeFormat = d3.timeFormat("%b %e %Y, %A");
 
-
+/* eslint-disable indent */
 	d3.select(".tooltip")
-		.html(() => `test, ${d3.event.pageY}, ${d3.event.pageX}`)
+		.html(`<div class="tooltip__header">${timeFormat(data.dates[idx])}</div>
+				<ul class="tooltip__list">
+				${params.data
+				.sort((a, b) => b.values[idx].price - a.values[idx].price)
+				.map(item => `  <li class="tooltip__list__item" style="color:${params.zScale(item.id)}" >
+									<span class="heavy">${item.id}: &nbsp</span><span class="normal">${item.values[idx].price.toFixed(2)}</span>
+								</li>`)
+				.join("")}
+				</ul>
+			`)
 		.style("opacity", "1")
 		.style("top", `${d3.event.pageY - 50}px`)
-		.style("left", `${d3.event.pageX + 15}px`);
+		.style("left", `${d3.event.pageX - 190}px`)
+		.transition()
+		.duration(250);
 
 	d3.select(".tooltipLine")
 		.attr("visibility", "visible")
-		.attr("d", `M ${xScale(data.dates[idx])} ${0} L ${xScale(data.dates[idx])} ${height} `);
+		.attr("d", `M ${params.xScale(data.dates[idx])} ${0} L ${params.xScale(data.dates[idx])} ${height} `);
 
 	d3.selectAll("circle")
 		.attr("title", idx)
-		.attr("cx", d => xScale(d.values[idx].date))
-		.attr("cy", d => yScale(d.values[idx].price));
+		.attr("cx", d => params.xScale(d.values[idx].date))
+		.attr("cy", d => params.yScale(d.values[idx].price));
 }
-/* eslint-disable prefer-arrow-callback */
-export function mouseClick(selection, idx, data, xScale, yScale) {
-	selection
-		.attr("title", idx)
-		.attr("cx", function a(d) { return xScale(d.values[idx].date); })
-		.attr("cy", function a(d) { return yScale(d.values[idx].price); });
-}
-/* eslint-enable */
 export function mouseEnter() {
 	d3.select(".tooltip")
-		.attr("visibility", "visible");
+		.style("display", "block");
 	d3.select(".tooltipLine")
 		.attr("visibility", "visible");
 	d3.selectAll("circle")
@@ -49,9 +53,34 @@ export function mouseEnter() {
 
 export function mouseLeave() {
 	d3.select(".tooltip")
-		.attr("visibility", "hidden");
+		.style("display", "none");
 	d3.select(".tooltipLine")
 		.attr("visibility", "hidden");
 	d3.selectAll("circle")
 		.attr("visibility", "hidden");
+}
+
+/* eslint-disable prefer-arrow-callback, indent */
+export function tooltipPointsUpdate(selection, idx, params) {
+	selection
+		.attr("title", idx)
+		.attr("cx", function a(d) { return params.xScale(d.values[idx].date); })
+		.attr("cy", function a(d) { return params.yScale(d.values[idx].price); });
+}
+
+export function tooltipUpdate(selection, idx, data, params) {
+	const timeFormat = d3.timeFormat("%b %e %Y, %A");
+
+	selection
+		.html(`<div class="tooltip__header">${timeFormat(data.dates[idx])}</div>
+			<ul class="tooltip__list">
+			${params.data
+			.sort((a, b) => b.values[idx].price - a.values[idx].price)
+			.map(item => `  <li class="tooltip__list__item" style="color:${params.zScale(item.id)}" >
+									<span class="heavy">${item.id}: &nbsp</span><span class="normal">${item.values[idx].price.toFixed(2)}</span>
+								</li>`)
+			.join("")}
+				</ul>
+		`);
+/* eslint-enable */
 }
